3a5623275dd4a1d83c2dcc34eb242742381e15ee
LyricLy <gulliverhanson@gmail.com>
2021-01-21 15:18:36 +1300

    Speed

--- bottom-encode.c
+++ bottom-encode.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include <wchar.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-
-// main code
 
 int main(void) {
     // build cache
@@ -13,16 +11,16 @@ int main(void) {
     int lengths[256];
     cache[0] = "❤️";
     lengths[0] = 6;
-    for (int i = 0; i < 256; i++) {
+    for (int i = 1; i < 256; i++) {
         char *b = malloc(40);
         int l = 0;
         int r = i;
         while (r) {
-        	if      (r >= 200){ r -= 200; memcpy(&b[l], "🫂", 4); l += 4; }
-            else if (r >= 50) { r -= 50;  memcpy(&b[l], "💖", 4); l += 4; }
-            else if (r >= 10) { r -= 10;  memcpy(&b[l], "✨", 3); l += 3; }
-            else if (r >= 5)  { r -= 5;   memcpy(&b[l], "🥺", 4); l += 4; }
-            else              { r -= 1;   b[l++] = ','; }
+            	if (r >= 200) { r -= 200; memcpy(&b[l], "🫂", 4); l += 4; }
+           else if (r >= 50)  { r -= 50;  memcpy(&b[l], "💖", 4); l += 4; }
+           else if (r >= 10)  { r -= 10;  memcpy(&b[l], "✨", 3); l += 3; }
+           else if (r >= 5)   { r -= 5;   memcpy(&b[l], "🥺", 4); l += 4; }
+           else               { r -= 1;   b[l++] = ','; }
         }
         memcpy(b+l, "👉👈", 8);
         l += 8;
@@ -32,12 +30,18 @@ int main(void) {
     }
 
     char buffer[8192];
-    setvbuf(stdout, buffer, _IOFBF, sizeof buffer);
+    unsigned int l = 0;
 
     // here goes
     int c;
     while ((c = getchar()) != EOF) {
-        fwrite(cache[c], 1, lengths[c], stdout);
+        if (l + lengths[c] > sizeof buffer) {
+            write(1, buffer, l);
+            l = 0;
+        }
+        memcpy(&buffer[l], cache[c], lengths[c]);
+        l += lengths[c];
     }
+    write(1, buffer, l);
     return 0;
 }
